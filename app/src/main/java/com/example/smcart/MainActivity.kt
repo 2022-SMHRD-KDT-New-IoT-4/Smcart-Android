30f242a9175235c67cbaa472e1f45ac850fe605e
ES0L <paraxxxol@gmail.com>
2023-03-06 17:39:03 +0900

    이솔 - flash화면, main화면 생성

---
+++ app/src/main/java/com/example/smcart/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.smcart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
