dd032978536c64ad0c807f19df36d024f54be0d2
ES0L <paraxxxol@gmail.com>
2023-03-06 17:50:17 +0900

    이솔 - push test용 액티비티 생성

---
+++ app/src/main/java/com/example/smcart/PushTestActivity.kt
@@ -0,0 +1,11 @@
+package com.example.smcart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PushTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_push_test)
+    }
+}
