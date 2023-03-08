508650824f054edacbab177a21adcb11fbdae588
ES0L <paraxxxol@gmail.com>
2023-03-08 20:48:49 +0900

    이솔 - 결제완료 페이지 제작

---
+++ app/src/main/java/com/example/smcart/PayDoneActivity.kt
@@ -0,0 +1,17 @@
+package com.example.smcart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PayDoneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pay_done)
+
+
+
+
+
+
+    }
+}
