75afe2d0bd38957831b49288d3fe5bfd95fff88e
poppedpigeon <ripcurr3nt@gmail.com>
2023-03-16 10:30:38 +0900

    지후씨의 기깔나는 프론트^^~ 장바구니 수량조절, 리스트 삭제, DB로 넣는 코드?

--- app/src/main/java/com/example/smcart/BasketActivity.kt
+++ app/src/main/java/com/example/smcart/BasketActivity.kt
@@ -3,14 +3,26 @@ package com.example.smcart
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.CompoundButton
 import android.widget.Switch
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONObject
 
 // 장바구니 페이지
 class BasketActivity : AppCompatActivity() {
+    private lateinit var queue: RequestQueue
+    private lateinit var request : StringRequest
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basket)
@@ -19,7 +31,35 @@ class BasketActivity : AppCompatActivity() {
         val btnSwitch = findViewById<Switch>(R.id.btnSwitch)
         val tvPay = findViewById<TextView>(R.id.tvPay)
 
+        queue  = Volley.newRequestQueue(this)
+        var url = "http://210.223.207.16:8081/cart/Product.do"
+
+
         val data = ArrayList<BasketVO>()
+        /*
+        request = object : StringRequest(
+            Method.POST, url,
+            Response.Listener<String> { response ->
+                Log.d("결과", response.toString())
+                val jsonArray = JSONArray(response)
+                for(i in 0..jsonArray.length()) {
+                    val name = jsonArray.getJSONObject(i).getString("prod_seq")
+                    val price = jsonArray.getJSONObject(i).getString("prod_name")
+                    val cnt = "1"
+                    val img = jsonArray.getJSONObject(i).getString("prod_img")
+                    data.add(BasketVO(name, price,cnt , img.toInt()))
+                }
+
+            },
+            Response.ErrorListener { error ->
+                Log.d("통신오류", error.printStackTrace().toString())
+            }) {
+            override fun getBodyContentType(): String {
+                return "application/json"
+            }
+
+        }
+        queue.add(request) */
         data.add(BasketVO("세척당근 3개입/봉", "3,600","2",R.drawable.carrot))
         data.add(BasketVO("대파 1kg /단", "4,200","1",R.drawable.greenonion))
         data.add(BasketVO("목우촌 국내산 돼지 앞다리 1000g/팩", "10,900","1",R.drawable.pork))
--- app/src/main/java/com/example/smcart/BasketAdapter.kt
+++ app/src/main/java/com/example/smcart/BasketAdapter.kt
@@ -11,8 +11,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class BasketAdapter(var context : Context, var data : ArrayList<BasketVO>):
-RecyclerView.Adapter<BasketAdapter.ViewHolder>(){
-    
+    RecyclerView.Adapter<BasketAdapter.ViewHolder>(){
     //테스트
     inner class ViewHolder(view : View) : RecyclerView.ViewHolder(view){
         val tvProdName : TextView
@@ -27,9 +26,9 @@ RecyclerView.Adapter<BasketAdapter.ViewHolder>(){
 
 
 
+
         init {
             tvProdName = view.findViewById(R.id.tvProdName)
-            tvProdPrice = view.findViewById(R.id.tvProdPrice)
             tvProdCnt = view.findViewById(R.id.tvProdCnt)
             imgProd = view.findViewById(R.id.imgProd)
             imgPlus = view.findViewById(R.id.imgPlus)
@@ -37,7 +36,34 @@ RecyclerView.Adapter<BasketAdapter.ViewHolder>(){
             btnHeart = view.findViewById(R.id.btnHeart)
             btnTrashcan = view.findViewById(R.id.btnTrashcan)
             btnProdCheck = view.findViewById(R.id.btnProdCheck)
+            tvProdPrice = view.findViewById(R.id.tvProdPrice)
 
+
+            imgPlus.setOnClickListener {
+                var cnt = tvProdCnt.text.toString().toInt()  // 원래 개수
+                val price = tvProdPrice.text.toString().toInt() / cnt // 원가
+                cnt +=1 // 추가된 개수
+                tvProdCnt.text = cnt.toString() // + 1 개 된 개수 출력
+                tvProdPrice.text = (price * cnt).toString()
+
+            }
+            imgMinus.setOnClickListener {
+                if(tvProdCnt.text.toString().toInt() != 1) {
+                    var cnt = tvProdCnt.text.toString().toInt()  // 원래 개수
+                    val price = tvProdPrice.text.toString().toInt() / cnt // 원가
+                    cnt -=1 // 추가된 개수
+                    tvProdCnt.text = cnt.toString() // + 1 개 된 개수 출력
+                    tvProdPrice.text = (price * cnt).toString()
+
+                }
+            }
+            btnTrashcan.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    data.removeAt(position)
+                    notifyItemRemoved(position)
+                }
+            }
         }
 
     }
@@ -50,13 +76,15 @@ RecyclerView.Adapter<BasketAdapter.ViewHolder>(){
 
     override fun onBindViewHolder(holder: BasketAdapter.ViewHolder, position: Int) {
         holder.tvProdName.text = data[position].name
-        holder.tvProdPrice.text = data[position].price
         holder.tvProdCnt.text = data[position].cnt
+        val price =  data[position].price.replace(",", "").toInt() * data[position].cnt.toInt()
+        holder.tvProdPrice.text = price.toString()
         holder.imgProd.setImageResource(data[position].img)
         holder.imgPlus.setImageResource(R.drawable.icon_plus)
         holder.imgMinus.setImageResource(R.drawable.icon_minus)
         holder.btnHeart.setImageResource(R.drawable.heart_gray)
         holder.btnTrashcan.setImageResource(R.drawable.trash_gray)
+
 
     }
 
